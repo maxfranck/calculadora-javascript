899202dc4753c9bc09c95915200376785fb4579f
maxfranck <max.franck@gmail.com>
2020-07-02 17:59:15 -0300

    Corrigindo bugs e polindo o código

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -1,15 +1,13 @@
 class CalcController {
-
     constructor() {
-
-        //Usamos 'this.' para criar uma variável em uma classe
-        //O underline representa uma classe privada
-        //Convencionando o uso do 'El' para referir que estamos pegando o elemento do HTML
         this._audio = new Audio('click.mp3'); //Fala qual audio tocar
         this._audioOnOff = true; //Padrão se o audio inicia ligado ou desligado
+        
+        this._finalResult = '';
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = []; //Array para guardar as operações
+
         this._locale = 'pt-BR'; // variavel para local
         this._displayCalcEl = document.querySelector('#display'); //Display da calculadora
         this._dateEl = document.querySelector('#date'); //Data da calculadora
@@ -18,16 +16,42 @@ class CalcController {
         this.initialize(); // Funçao de inicialização
         this.initButtonsEvents(); // Função de eventos dos botões
         this.initKeyboard(); // Função de eventos dos botões do teclado
-
     }
 
-    //Funções dentro de uma classe recebe o nome de método
+    initialize() {
+        this.setDisplayDateTime();
 
+        //Função executada em um intervalo de tempo em milissegundos
+        setInterval(()=>{
+            this.setDisplayDateTime();
+        }, 1000);
 
+        this.setLastNumberToDisplay();
+        this.pasteFromClipboard();
+
+        //Se der duplo click no botão AC o audio liga ou desliga
+        document.querySelectorAll('.btn-ac').forEach(btn =>{
+            btn.addEventListener('dblclick', e =>{
+                this.toggleAudio();
+            });
+        });
+    }
+
+    //AUDIO ON AND OFF
+    toggleAudio() {
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    //PLAY AUDIO
+    playAudio() {
+        if (this._audioOnOff) {
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
+    }
+
+    //COPY DATA FROM DISPLAY TO THE CLIPBOARD
     copyToClipboard() {
-
-        //Criando um input aqui pois estamos usando SVG. 
-        //Se criar em html não precisa criar o input aqui.
         let input = document.createElement('input');
 
         input.value = this.displayCalc;
@@ -39,82 +63,31 @@ class CalcController {
         document.execCommand("Copy");
 
         input.remove();
-
     }
 
+    //PASTE DATA FROM CLIPBOARD
     pasteFromClipboard() {
-
-        document.addEventListener('paste', e => {
-
+        document.addEventListener('paste', e =>{
             let text = e.clipboardData.getData('Text');
-
             this.displayCalc = parseFloat(text);
-
         });
     }
 
-    //Método de inicialização - Principal
-    initialize() {
-
-        this.setDisplayDateTime();
-
-        //Função executada em um intervalo de tempo em milissegundos
-        setInterval(()=> {
-
-            this.setDisplayDateTime();
-
-        }, 1000);
-
-        this.setLastNumberToDisplay();
-        this.pasteFromClipboard();
-
-        //Se der duplo click no botão AC o audio liga ou desliga
-        document.querySelectorAll('.btn-ac').forEach(btn => {
-
-            btn.addEventListener('dblclick', e => {
-
-                this.toggleAudio();
-
-            });
-
-        });
-
-    }
-
-    //Liga e desliga audio
-    toggleAudio() {
-
-        this._audioOnOff = !this._audioOnOff;
-
-    }
-
-    //Toca audio
-    playAudio() {
-
-        if (this._audioOnOff) {
-
-            this._audio.currentTime = 0;
-            this._audio.play();
-
-        }
-
-    }
-
-    //Utilizando o teclado numérico
+    //SETTING UP KEYBOARD EVENTS
     initKeyboard() {
-
-        document.addEventListener('keyup', e => {
-
-            this.playAudio(); //Colocar ordem de tocar audio ao clicar na tecla
+        document.addEventListener('keyup', e =>{
+            this.playAudio();
 
             switch(e.key) {
 
                 case'Escape':
                     this.clearAll();
                     break;
+
                 case 'Backspace':
                     this.clearEntry();
                     break;
+
                 case '+':
                 case '-':
                 case '/':
@@ -122,14 +95,17 @@ class CalcController {
                 case '%':
                     this.addOperation(e.key);
                     break;
+
                 case 'Enter':
                 case '=':
                     this.calc();
                     break;
+
                 case '.':
                 case ',':
                     this.addDot();
                     break;
+
                 case '0': 
                 case '1':
                 case '2':
@@ -142,268 +118,236 @@ class CalcController {
                 case '9':
                     this.addOperation(parseInt(e.key));
                     break;
+
                 case 'c':
-                    if (e.ctrlKey) this.copyToClipboard(); //Add tecla Ctrl+C para copiar valor
+                    if (e.ctrlKey) this.copyToClipboard();
                     break;
-
             }
-
         });
-
     }
 
-    //Método para separar o evento de click e drag
+    //METHOD TO SET EVENTS TO ALL BUTTONS
     addEventListenerAll(element, events, fn) {
-
-        events.split(' ').forEach(event => {
-
+        events.split(' ').forEach(event =>{
             element.addEventListener(event, fn, false);
-
         });
-
     }
 
+    //FUNCTION: RESET THE OPERATION TO EMPTY
     clearAll() {
-
         this._operation = [];
         this._lastNumber = '';
         this._lastOperator = '';
-
         this.setLastNumberToDisplay();
-
     }
 
+    //FUNCTION: ERASE THE LAST ENTRY OF THE OPERATION
     clearEntry() {
-
         this._operation.pop();
-
         this.setLastNumberToDisplay();
-
     }
 
+    //DISPLAYS ERROR
+    setError() {
+        this.displayCalc = "Error";
+    }
+
+    //GETS THE LAST OPERATION OF THE ARRAY
     getLastOperation() {
-
         return this._operation[this._operation.length-1];
-
     }
 
-    setLastOperation(value) {
-
-        this._operation[this._operation.length-1] = value;
-
-    }
-
-    //Verifica se é um operador ou não
+    //VALIDATES IF LAST VALUE IS AN OPERATOR
     isOperator(value) {
-
-        //indexOf procura um valor dentro do array
         if (['+', '-', '*', '/', '%'].indexOf(value) > -1) {
-
             return true;
-
         } else {
-
             return false;
+        }
+    }
 
+    //SET THE LAST OPERATION TO THE INPUT VALUE
+    setLastOperation(value) {
+        this._operation[this._operation.length-1] = value;
+    }
+
+    //PUSH VALUE TO OPERATION
+    pushOperation(value) {
+        this._operation.push(value);
+
+        if(this._finalResult){
+            this._finalResult = '';
         }
 
+        if (this._operation.length > 3) {
+            this.calc();
+        }
     }
 
-    //Mudar de array para string juntando a operação
+    //RETURN THE RESULT OF THE CALCULATION
     getResult() {
-
-        return eval(this._operation.join(""));
-
+        //Add Try Catch para pegar algum erro
+        try {
+            return eval(this._operation.join(""));
+        } catch(e) {
+            setTimeout(() =>{
+                this.setError();
+            }, 1); 
+        }
     }
 
+    //CALCULATES THE OPERATION
     calc() {
-
         let last = '';
 
         this._lastOperator = this.getLastItem();
 
         if (this._operation.length < 3) {
-
             let firstItem = this._operation[0];
             this._operation = [firstItem, this._lastOperator, this._lastNumber];
-
         }
 
         if (this._operation.length > 3) {
-
             last = this._operation.pop();
             this._lastNumber = this.getResult();
-
         } else if (this._operation.length == 3) {
-
             this._lastNumber = this.getLastItem(false);
-
         }
 
         let result = this.getResult();
 
         //Calcula porcentagem
         if (last == '%') {
-
             result /= 100;
             this._operation = [result];
-
         } else {
-
         //retornar a operação para array
         this._operation = [result];
         if (last) this._operation.push(last);
-
         }
 
+        this._finalResult = this.getResult();
+        
         this.setLastNumberToDisplay();
-
     }
 
-    pushOperation(value) {
-
-        this._operation.push(value);
-
-        if (this._operation.length > 3) {
-
-            this.calc();
-
-        }
-
-    }
-
+    //RETURN THE LAST ITEM OF THE OPERATION
     getLastItem(isOperator = true) {
-
         let lastItem;
 
         for (let i = this._operation.length-1; i>= 0; i--) {
-
             if (this.isOperator(this._operation[i]) == isOperator) {
-
                 lastItem = this._operation[i];
                 break;
-
             }
-
         }
 
-        if (!lastItem) {
-
+        if (lastItem == 0){
+            return lastItem;
+        } else if (!lastItem) {
             lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
-
         }
 
         return lastItem;
-
     }
 
-    //Atualiza e mostra número no display
+    //SHOW LAST NUMBER ON DISPLAY
     setLastNumberToDisplay() {
-
         let lastNumber = this.getLastItem(false);
 
         if (!lastNumber) lastNumber = 0;
 
         this.displayCalc = lastNumber;
-
     }
     
-    //Adicionando nova operação
+    //ADD TO THE OPERATION
     addOperation(value) {
-
-        console.log('A', this.getLastOperation());
-
         if (isNaN(this.getLastOperation())) {
-
-            //Troca o operador caso já tenha clicado em um anteriormente
             if (this.isOperator(value)) {
-
                 this.setLastOperation(value);
-
             } else {
-
                 this.pushOperation(value);
                 this.setLastNumberToDisplay();
-
             }
-
         } else {
-
             if (this.isOperator(value)) {
-
                 this.pushOperation(value);
-
             } else {
-
-                let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(newValue);
-                this.setLastNumberToDisplay();
-
+                if(!this._finalResult){
+                    let newValue = this.getLastOperation().toString() + value.toString();
+                    this.setLastOperation(newValue);
+                    this.setLastNumberToDisplay();
+                } else {
+                    this._lastNumber = '';
+                    this._lastOperator = '';
+                    this._operation = [];
+ 
+                    this.addOperation(value);
+                    this.setLastNumberToDisplay();
+                }
             }
-
         }
-
     }
 
-    setError() {
-
-        this.displayCalc = "Error";
-
-    }
-
+    //METHOD TO TURN NUMBERS INTO FLOAT TYPE
     addDot() {
-
         let lastOperation = this.getLastOperation();
 
         if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
 
-        if (this.isOperator(lastOperation) || !lastOperation) {
-
+        if(this._finalResult){
+            this._operation = [];
+            this.pushOperation("0.");
+        } else if (this.isOperator(lastOperation) || !lastOperation || lastOperation == undefined) {
             this.pushOperation('0.')
-
         } else {
-
             this.setLastOperation(lastOperation.toString() + '.');
-
         }
 
         this.setLastNumberToDisplay();
-
     }
 
+    //SETTING ATTRIBUTES FOR THE BUTTONS
     execBtn(value){
-
         this.playAudio();
 
         switch(value) {
-
             case'ac':
                 this.clearAll();
                 break;
+
             case 'ce':
                 this.clearEntry();
                 break;
+
             case 'soma':
                 this.addOperation('+');
                 break;
+
             case 'subtracao':
                 this.addOperation('-');
                 break;
+
             case 'divisao':
                 this.addOperation('/');
                 break;
+
             case 'multiplicacao':
                 this.addOperation('*');
                 break;
+
             case 'porcento':
                 this.addOperation('%');
                 break;
+
             case 'igual':
                 this.calc();
                 break;
+
             case 'ponto':
                 this.addDot();
                 break;
+
             case '0': 
             case '1':
             case '2':
@@ -416,104 +360,79 @@ class CalcController {
             case '9':
                 this.addOperation(parseInt(value));
                 break;
+            
             default:
                 this.setError();
                 break;
-
         }
-
     }
 
-    // Função de Inicialização dos Botões - lê do HTML os dados da calculadora 
+    //SETUP THE BUTTON EVENTS
     initButtonsEvents() {
-
         //pega todas as tags g que são filhas de buttons e parts
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
 
-        //Foreach para percorrer todos os botões
-        buttons.forEach((btn, index)=> {
-
+        buttons.forEach((btn, index)=>{
             //Evento mouse click e drag
-            this.addEventListenerAll(btn, 'click drag', e=> {
-
+            this.addEventListenerAll(btn, 'click drag', e=>{
                 let textBtn = btn.className.baseVal.replace("btn-", "");
-
                 this.execBtn(textBtn);
-
             });
+
             //Adiciona mão ao passar o mouse sobre os botões
-            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e=> {
-
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e=>{
                 btn.style.cursor = "pointer";
-
             });
-
         });
-
     }
 
+    //SETS THE DATE AND TIME TO DISPLAY
     setDisplayDateTime() {
-
-        //Pegando data e hora no formato definodo no locale
         this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
-
             day: '2-digit',
             month: 'short',
             year: 'numeric'
-
         });
 
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
-
     }
 
-    //Por convenção deve-se usar geters (get) e seters (set) para pegar e setar valores de uma classe privada
+    //GETTERS AND SETTERS
     get displayTime() {
-
         return this._timeEl.innerHTML;
-
     }
     
     set displayTime(value) {
-
         return this._timeEl.innerHTML = value;
-
     }
     
     get displayDate() {
-
         return this._dateEl.innerHTML;
-
     }
     
     set displayDate(value) {
-
         return this._dateEl.innerHTML = value;
-
     }
 
     get currentDate() {
-
         return new Date();
-
     }
 
     set currentDate(value) {
-
         return this._currentDate = value;
-
     }
 
     get displayCalc() {
-
         return this._displayCalcEl.innerHTML;
-
     }
 
     set displayCalc(value) {
+        if (value.toString().length > 10) {
+            this.setError();
+            return false;
+        }
 
-        this._displayCalcEl.innerHTML = value;
-
+        return this._displayCalcEl.innerHTML = value;
     }
 
 }
